cf99d13ee5ef7e69eaeeb19035a36156c79aab8c
mzw7p2 <dmitry.kokotov@gm.com>
2024-05-29 17:38:58 -0400

    add setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -sf .bash/.bashrc ~/.bashrc
+ln -sf .bash/.bash_logout ~/.bash_logout
+ln -sf .bash/.bash_history ~/.bash_history
+ln -sf .bash/.profile ~/.profile
+source ~/.bashrc
+
