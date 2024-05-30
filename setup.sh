0ff79ca1dfe52e315f1f138cc0603f3ea3913687
mzw7p2 <dmitry.kokotov@gm.com>
2024-05-29 20:31:40 -0400

    oh-my-bash install

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)"
 ln -sf .bash/.bashrc ~/.bashrc
 ln -sf .bash/.bash_logout ~/.bash_logout
 ln -sf .bash/.bash_history ~/.bash_history
